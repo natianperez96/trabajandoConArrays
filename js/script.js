c9f14b0b48d0c8b509dc232e968fa01b23038512
Antonella <natianperez@outlook.com>
2023-08-26 22:44:50 -0300

    Trabajo terminado

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+// Función que recibe por parámetro un array y muestra sus elementos en pantalla
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  // Más info de forEach => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  const result = strangeArray.filter((string) => typeof string === "string");
+  const mapped = result.map(function (el, i) {
+    return { index: i, value: el.toUpperCase() };
+  });
+  mapped.sort(function (a, b) {
+    if (a.value > b.value) {
+      return 1;
+    }
+    if (a.value < b.value) {
+      return -1;
+    }
+    return 0;
+  });
+  const resultado = mapped.map(function (el) {
+    return result[el.index];
+  });
+  showList(resultado);
+  // Sugerencia de cómo mostrar el array => showList(strangeArray);
+});
